900a1871604e765a6fe7caa163065571f775e8f3
jessica <laylaruan0927@gmail.com>
2023-12-15 16:09:35 -0500

    build simeple html form

---
+++ app.js
@@ -0,0 +1,6 @@
+// 1.Using curl, make a GET request to the icanhazdadjoke.com API to find all jokes involving the word “pirate”
+
+curl -H "Accept: application/json" "https://icanhazdadjoke.com/search?term=pirate"
+
+// 2.Use dig to find what the IP address is for icanhazdadjoke.com
+dig icanhazdadjoke.com
